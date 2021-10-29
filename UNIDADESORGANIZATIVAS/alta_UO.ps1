1aae0a5e4a10097661f85f989242ba6e6838fb86
alurue9140 <91330447+alurue9140@users.noreply.github.com>
2021-10-29 15:23:12 +0200

    Delete UNIDADESORGANIZATIVAS directory

--- UNIDADESORGANIZATIVAS/alta_UO.ps1
+++
@@ -1,9 +0,0 @@
-﻿param($dominio,$suf)
-$domainComponent="dc="+$dominio+",dc="+$suf
-Write-Host $domainComponent
-$ficheroCsvUO=Read-Host "Introduce el fichero csv de UO's:"
-$fichero = import-csv -Path $ficheroCsvUO -delimiter :
-foreach($line in $fichero)
-{
-   New-ADOrganizationalUnit -Description:$line.Description -Name:$line.Name -Path:$domainComponent -ProtectedFromAccidentalDeletion:$true
-}
